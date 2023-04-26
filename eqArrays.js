8c7840a6789d1114e477d0c59a33cd718c05e21c
ccacook98 <caleb_cook@outlook.com>
2023-04-26 16:30:32 +0000

    Added the eqArrays.js file.

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`[O] Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`[X] Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if(arr1.length != arr2.length) {
+    return false;
+  }
+  for(var i = 0; i < arr1.length; i++) {
+    if(arr1[i] != arr2[i]) {
+      return false;
+    }
+  }
+  return true; //return true if neither of the above return false
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([3, 2, 1], [1, 2, 3]), false); // => should FAIL
+
