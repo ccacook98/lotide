8c790b94ca053e8c7ef6d8d23f18f5d5bd1e88fa
ccacook98 <caleb_cook@outlook.com>
2023-05-11 15:38:25 +0000

    Initial commit for countOnly.js: added skeleton code and test cases

---
+++ countOnly.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`[O] Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`[X] Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
