dc2bce11497850ee2ea2f244f139dc782aa6b117
ccacook98 <caleb_cook@outlook.com>
2023-05-13 16:46:16 +0000

    Added test code to countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -21,4 +21,11 @@ const countLetters = function(lettersToCount) {
   return results; 
 };
 
-console.log(countLetters("lighthouse in the house"));
+const output = countLetters("lighthouse in the house");
+const expected = { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 };
+const keys = Object.keys(output);
+
+for (const key of keys) {
+  assertEqual(output[key], expected[key]);
+}
+
