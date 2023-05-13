7482426813da28f00bd6846b4267aa0063eba659
ccacook98 <caleb_cook@outlook.com>
2023-05-13 16:20:27 +0000

    Initial commit for countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`[O] Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`[X] Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(lettersToCount) {
+  const results = {};
+
+  for (const character of lettersToCount) {
+    if (results[character]) {
+      results[character]++;
+    } else {
+      results[character] = 1;
+    }
+  }
+  return results; 
+};
