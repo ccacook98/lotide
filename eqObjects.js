75f25c84b6fae95da8e634359fd00224ebc38b25
ccacook98 <caleb_cook@outlook.com>
2023-05-14 05:05:00 +0000

    Initial commit for eqObjects.js with partial implementaton of main function.

---
+++ eqObjects.js
@@ -0,0 +1,8 @@
+const eqObjects = function(object1, object2) {
+  //Retrieve lists of the keys present in each object and store them in arrays
+  const obj1Keys = Object.keys(object1);
+  const obj2Keys = Object.keys(object2);
+  //Proceed on
+  if (obj1Keys.length === obj2Keys.length) {
+    
+};
