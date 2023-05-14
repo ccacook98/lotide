0f3b79078fded437ef8253e6a14ee398b1e9cb9a
ccacook98 <caleb_cook@outlook.com>
2023-05-14 04:41:55 +0000

    Added findKeyByValue.js.

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`[O] Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`[X] Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  for (const key in obj); {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+  //only occurs if no match is found
+  return undefined;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
