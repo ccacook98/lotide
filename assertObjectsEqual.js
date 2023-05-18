ae4526fd60cb3fe63c19f2a20dcb324919e669d7
ccacook98 <caleb_cook@outlook.com>
2023-05-18 13:47:26 +0000

    assertObjectsEqual.js: Added testing code and fixed a few bugs.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -45,6 +45,20 @@ const assertObjectsEqual = function(actual, expected) {
   if (eqObjects(actual, expected)) { 
     console.log(`[O] Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`[X] Assertion Failed: ${inspect(actual)} === ${inspect(expected)}`);
+    console.log(`[X] Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+assertObjectsEqual(shirtObject , anotherShirtObject); // => true
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+assertObjectsEqual(shirtObject , longSleeveShirtObject); // => false
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertObjectsEqual(multiColorShirtObject  , anotherMultiColorShirtObject); // => true
+
+const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertObjectsEqual(multiColorShirtObject  , longSleeveMultiColorShirtObject); // => false
