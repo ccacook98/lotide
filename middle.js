c66385acf910b0ea4b5d51ccc45a4380dff12175
ccacook98 <caleb_cook@outlook.com>
2023-05-31 19:38:21 +0000

    Separated and modularized middle().

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true; //return true if neither of the above return false
-};
-
-const assertArraysEqual = function(actual, expected) {
-  let arraysEqual = eqArrays(actual, expected);
-  if (arraysEqual) {
-    console.log(`[O] Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`[X] Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(array) {
   if (array.length < 3) {
     return [];
@@ -31,9 +10,4 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
