f537f1a8d553e958d333c66907a5af8ba56d6724
ccacook98 <caleb_cook@outlook.com>
2023-05-31 18:58:41 +0000

    findKey.js: Added test code and fixed a small bug)

--- findKey.js
+++ findKey.js
@@ -8,10 +8,21 @@ const assertEqual = function(actual, expected) {
 
 const findKey = function(obj, func) {
   let output;
-  for (const key of obj) {
-    if (func(key)) {
+  for (const key in obj) {
+    if (func(obj[key])) {
       output = key;
+      break;
     }
   }
   return output;
 }
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+	   );
