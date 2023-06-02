9e63540b4b5903a652a19f1ce70861960610c2ed
ccacook98 <caleb_cook@outlook.com>
2023-06-02 14:03:54 +0000

    Converted middleTest.js to use Mocha.

--- tests/middleTest.js
+++ tests/middleTest.js
@@ -1,9 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.sameMembers(middle([1]), []);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.sameMembers(middle([1, 2, 3]), [2]);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.sameMembers(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.sameMembers(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.sameMembers(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
