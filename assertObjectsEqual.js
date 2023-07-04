c58eeab0023486edde88643f0471beb09cc4a8b1
ccacook98 <caleb_cook@outlook.com>
2023-07-04 15:46:38 +0000

    Separated test code for assertObjectsEqual() into its own file under the tests/ directory.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -11,17 +11,3 @@ const assertObjectsEqual = function(actual, expected) {
 };
 
 module.exports = assertObjectsEqual;
-
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-assertObjectsEqual(shirtObject , anotherShirtObject); // => true
-
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-assertObjectsEqual(shirtObject , longSleeveShirtObject); // => false
-
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-assertObjectsEqual(multiColorShirtObject  , anotherMultiColorShirtObject); // => true
-
-const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-assertObjectsEqual(multiColorShirtObject  , longSleeveMultiColorShirtObject); // => false
