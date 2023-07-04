ff610a6a3ddb7055c4a5a27bd7e1c0797adf00ba
ccacook98 <caleb_cook@outlook.com>
2023-07-04 05:51:54 +0000

    Generated a new index.js file with all the functions in it (that was a fun little exercise involving some small shell scripts)

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-// index.js
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
+//index.js
+
+
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findKeyByValue = require("./findKeyByValue");
+const findKey = require("./findKey");
+const flatten = require("./flatten");
+const head = require("./head");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const middle = require("./middle");
+const tail = require("./tail");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
 
 module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKeyByValue,
+  findKey,
+  flatten,
   head,
-  tail,
+  letterPositions,
+  map,
   middle,
+  tail,
+  takeUntil,
+  without
 };
